9268e93b53d64947da0293ade19ca943cafefdec
Shlomi Shitrit <124shlomis@gmail.com>
2020-04-07 11:07:15 +0300

    downloaded files from course server
    downloaded all the files for checking

--- testCompile.sh
+++ testCompile.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-# this script makes compilation and save all the compilation's data into a log file.
+# this script makes compilation and save all the compilation data into a log file.
 # the script gets 2 parameters:
 # parameter 1 - the name of the log file - for user choice.
 # parameter 2 - the name of the program which the user would like to create.
-# the currnet directory should contain this name.
-gcc -Wall -o "$2" "$2.c" > "$1.log"
+gcc -Wall -o "$2" "{$2}.c" > "$1.log"
