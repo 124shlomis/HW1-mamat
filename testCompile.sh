1e5df9c74394a99377bb68477a68b43e3930222b
Shlomi Shitrit <124shlomis@gmail.com>
2020-04-07 11:38:42 +0300

    Update Script

--- testCompile.sh
+++ testCompile.sh
@@ -3,7 +3,7 @@
 # the script gets 2 parameters:
 # parameter 1 - the name of the log file - for user choice.
 # parameter 2 - the name of the program which the user would like to create.
-gcc -Wall -o "$2" "{$2}.c" > "$1.log"
+gcc -Wall -o "$2" "$2.c" &> "$1.log"
 if [ $? -ne 0 ]
 then
     echo "Compile failed"
