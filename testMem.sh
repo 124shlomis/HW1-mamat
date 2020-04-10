e65e8b63f5cd669dc30d28a8d0f185b2d9e45c4c
Shlomi Shitrit <124shlomis@gmail.com>
2020-04-10 09:48:00 +0300

    Update testMem.sh

--- testMem.sh
+++ testMem.sh
@@ -3,13 +3,12 @@
 # parameter 1 - name of a log file will be create - for user choice
 # parameter 2 - name of a program to check
 # parameter 3 - input file for the program
-valgrind --tool=memcheck --leak-check=full ./$2 < $3 > "$1.log"
-grep -q "ERROR SUMMARY: 0 errors" "$1.log"
+valgrind --tool=memcheck --leak-check=full --error-exitcode=1 ./$2 $3 &> "$1.log"
 ret=$?
 if (( ret == 0 )) ; then
 	echo "Memory test passed"
-	exit $ret 
+	exit 0 
 elif (( ret != 0 )); then
 	echo "Memory test failed"
-	exit $ret
+	exit 1
 fi
