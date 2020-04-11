44436f7775aecb1a62bd438222978d0cd59f779d
Shlomi Shitrit <124shlomis@gmail.com>
2020-04-11 20:33:54 +0300

    Update testCompile.sh
    added some notes

--- testCompile.sh
+++ testCompile.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+
 # this script makes compilation and save all the compilation data into a log file.
 # the script gets 2 parameters:
 # parameter 1 - the name of the log file - for user choice.
 # parameter 2 - the name of the program which the user would like to create.
+
 gcc -Wall -o "$2" "$2.c" &> "$1.log"
-if [ $(grep -c "$2.c" $1.log) -ne 0 ]
+if [ $(grep -c "$2.c" $1.log) -ne 0 ] # looking for compilation errors
 then
     echo "Compile failed"
     exit 1
