62ee009f3e13efefb9e4c6ec9d47e5bf902e31fc
Shlomi Shitrit <124shlomis@gmail.com>
2020-04-11 21:35:52 +0300

    Update testMem.sh
    using grep command

--- testMem.sh
+++ testMem.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
+
 # this script check if there is a memory leaking of the given program
 # parameter 1 - name of a log file will be create - for user choice
 # parameter 2 - name of a program to check
 # parameter 3 - input file for the program
-valgrind --tool=memcheck --leak-check=full --error-exitcode=1 ./$2 $3 &> "$1.log"
-ret=$?
-if (( ret == 0 )) ; then
+
+valgrind --tool=memcheck ./"$2" < "$3" &> "$1.log"
+ret=`grep -c "ERROR SUMMARY: 0 errors" "$1.log"`
+if  [ $ret != 0 ] ; then
 	echo "Memory test passed"
 	exit 0 
-elif (( ret != 0 )); then
+elif [ $ret == 0 ]; then
 	echo "Memory test failed"
 	exit 1
 fi
